02727282f8ab370d861713f1180b5bf650f4fdf5
warissara phanrod <warissara.ph@kkumail.com>
2025-03-19 10:45:29 +0700

    help

--- app.js
+++ app.js
@@ -49,6 +49,8 @@ var app = express();
 // View engine setup
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('trust proxy', 1);
+
 
 app.engine('html', ejs.renderFile);
 
